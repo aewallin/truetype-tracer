f8498d8419709240d1383085b4d516965d50cb61
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-01-17 14:55:00 +0200

    clean

--- ttt_py.cpp
+++ ttt_py.cpp
@@ -44,12 +44,7 @@ static std::string dxf0() {
 
 static std::string ngc0() {
     return ngc1("Hello World.");
-}    
-
-/*
-std::string version() {
-    return "TTT TrueType tracer, c++ port!";
-}*/
+}
 
 static std::string ttt_with_writer(std::string str, Writer* wr) {
     //Writer* wr = new NGC_Writer( TTFONT, str, false, 0.003, false );
@@ -89,35 +84,5 @@ BOOST_PYTHON_MODULE(ttt) {
     bp::class_< SEG_Writer, bp::bases<Writer> >("SEG_Writer")
         .def( "get_segments", &SEG_Writer::get_segments)
     ;
-    
-    /*
-    bp::class_< VoronoiDiagram_py, bp::bases<VoronoiDiagram> >("VoronoiDiagram")
-        .def(bp::init<double, unsigned int>())
-        .def("addVertexSite",  &VoronoiDiagram_py::insert_point_site1 )
-        .def("addVertexSite",  &VoronoiDiagram_py::insert_point_site2 )
-        .def("addLineSite",  &VoronoiDiagram_py::insert_line_site2 ) // takes one argument
-        .def("addLineSite",  &VoronoiDiagram_py::insert_line_site3 ) // takes two arguments
-        .def("getGenerators",  &VoronoiDiagram_py::getGenerators)
-        .def("getEdgesGenerators",  &VoronoiDiagram_py::getEdgesGenerators)
-        .def("getVoronoiVertices",  &VoronoiDiagram_py::getVoronoiVertices) 
-        .def("getFarVoronoiVertices",  &VoronoiDiagram_py::getFarVoronoiVertices)
-        .def("getFarRadius",  &VoronoiDiagram_py::get_far_radius)
-        .def("getVoronoiEdges",  &VoronoiDiagram_py::getVoronoiEdges)
-        .def("getVoronoiEdgesOffset",  &VoronoiDiagram_py::getVoronoiEdgesOffset)
-        .def("numPointSites", &VoronoiDiagram_py::num_point_sites)
-        .def("numLineSites", &VoronoiDiagram_py::num_line_sites)
-        .def("numVertices", &VoronoiDiagram_py::num_vertices)
-        .def("numSplitVertices", &VoronoiDiagram_py::num_split_vertices)
-        .def("__str__", &VoronoiDiagram_py::print)
-        .def("version", &VoronoiDiagram_py::version)
-        .def("reset_vertex_count", &VoronoiDiagram_py::reset_vertex_count)
-        .def("setEdgePoints", &VoronoiDiagram_py::set_edge_points)
-        .def("setEdgeOffset", &VoronoiDiagram_py::set_null_edge_offset)
-        .def("debug_on", &VoronoiDiagram_py::debug_on)
-        .def("check", &VoronoiDiagram_py::check)
-        .staticmethod("reset_vertex_count")
-        .def("getStat", &VoronoiDiagram_py::getStat)
-        .def("getFaceStats", &VoronoiDiagram_py::getFaceStats)
-    ;
-    */
+
 }
