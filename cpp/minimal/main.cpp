c49c9d55c93f690c6c875d727db801517e5813d5
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-01-28 15:30:06 +0200

    minimal c++ example.

---
+++ cpp/minimal/main.cpp
@@ -0,0 +1,19 @@
+
+#include <string>
+#include <iostream>
+
+#include <ttt/ttt.hpp>
+#include <ttt/ngc_writer.hpp>
+
+#define TTFONT "/usr/share/fonts/truetype/freefont/FreeSerif.ttf"
+
+// ttt example program
+int main() {
+    NGC_Writer my_writer;
+    Ttt t( &my_writer, "Test", false , TTFONT );
+    
+    std::string out = t.get_output();
+    std::cout << out;
+
+    return 0;
+}
